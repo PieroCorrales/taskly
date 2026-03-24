9d6373fbf65172ec87e9d11671cef00d9def1348
piecorra <piecorra@student.42madrid.com>
2026-03-24 07:10:47 +0100

    feat: API REST completa con autenticación y CRUD de tareaas

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,42 +3,67 @@ const router = express.Router();
 const Task = require('../models/Task');
 const protect = require('../middleware/auth');
 
-//listar tareas ya creadas
-router.get('/', protect, async(req, res) => {
-    try{
-        const task = await Task.find({ user: req.user.id});
-        res.json(tasks);
-    } catch (error){
-        res.status(500).json({ message: 'Error del servidor' });
-    }
+router.get('/', protect, async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-//crear tarea
 router.post('/', protect, async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const task = await Task.create({
-            title,
-            description,
-            user: req.user.id
-        });
-        res.status(201).json(task);
-    } catch(error) {
-        res.status(500).json({ message: 'Error del servidor' });
-    }
+  try {
+    const { title, description } = req.body;
+    const task = await Task.create({
+      title,
+      description,
+      user: req.user.id
+    });
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-//actualizar tareas
-router.put('/:id', protect, async(req, res) => {
-    try {
-
-
-    } catch (error) {
-
+router.put('/:id', protect, async (req, res) => {
+  try {
+    console.log('req.user:', req.user);
+    console.log('params id:', req.params.id);
+    const task = await Task.findById(req.params.id);
+    console.log('tarea encontrada:', task);
+    if (!task) {
+      return res.status(404).json({ message: 'Tarea no encontrada' });
     }
+    if (task.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'No autorizado' });
+    }
+    const updated = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updated);
+  } catch (error) {
+    console.log('ERROR PUT:', error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
-
-//eliminar tareas
+router.delete('/:id', protect, async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Tarea no encontrada' });
+    }
+    if (task.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'No autorizado' });
+    }
+    await task.deleteOne();
+    res.json({ message: 'Tarea eliminada' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
